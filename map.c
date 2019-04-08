a75090c022d2251f38846f040409dda2cc45d1f8
idan shenfeld <idanshen@campus.technion.ac.il>
2019-04-08 21:50:13 +0300

    added h and c basic files and create struct and mapcreate

--- map.c
+++ map.c
@@ -1,7 +1,40 @@
+#include "map.h"
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
 
-int main(){
+#define ERROR -1
 
-    printf("hello world");
-    return 0;
+/** TODO */
+typedef struct node_t {
+    void *key;
+    void *data;
+    struct node_t *next;
+} *Node;
+
+/** TODO */
+struct Map_t {
+    Node *base;
+    copyMapDataElements copyData;
+    copyMapKeyElements copyKey;
+    freeMapDataElements freeData;
+    freeMapKeyElements freeKey;
+    compareMapKeyElements compareKey;
+};
+
+Map mapCreate(copyMapDataElements copyDataElement,
+              copyMapKeyElements copyKeyElement,
+              freeMapDataElements freeDataElement,
+              freeMapKeyElements freeKeyElement,
+              compareMapKeyElements compareKeyElements) {
+    Map map = malloc(sizeof(*map));
+    if (map == NULL) {
+        return NULL;
+    }
+    map->base = NULL;
+    map->copyData = copyDataElement;
+    map->copyKey = copyKeyElement;
+    map->freeData = freeDataElement;
+    map->freeKey = freeKeyElement;
+    map->compareKey = compareKeyElements;
 }
