a6eab47f43d01f15c3964908cbec51e408d594f7
Roy <roy.bernea@campus.technion.ac.il>
2019-04-11 11:56:30 +0300

    added new functions
    added getSize,mapContains,mapClear,mapDestroy,mapCopy

--- map.c
+++ map.c
@@ -22,6 +22,7 @@ struct Map_t {
     compareMapKeyElements compareKey;
 };
 
+
 Map mapCreate(copyMapDataElements copyDataElement,
               copyMapKeyElements copyKeyElement,
               freeMapDataElements freeDataElement,
@@ -37,4 +38,67 @@ Map mapCreate(copyMapDataElements copyDataElement,
     map->freeData = freeDataElement;
     map->freeKey = freeKeyElement;
     map->compareKey = compareKeyElements;
+}
+
+int mapGetSize(Map map){
+    if (map==NULL){
+        return ERROR;
+    }
+    int counter=1;
+    Node next_ptr=(*(map->base))->next;
+    while(next_ptr!=NULL){
+        counter+=1;
+        next_ptr=next_ptr->next;
+    }
+    return counter;
+
+}
+
+bool mapContains(Map map, MapKeyElement element){
+    if(map==NULL || element==NULL){
+        return false;
+    }
+    Node first_node=(*(map->base));
+    MapKeyElement current_key=first_node->key;
+    Node next_node=first_node->next;
+    while(next_node!=NULL){
+        if((map->compareKey(current_key,element))==0){
+            return true;
+        }
+        next_node=next_node->next;
+    }
+    return false;
+}
+
+MapResult mapClear(Map map){
+    if(map==NULL){
+        return MAP_NULL_ARGUMENT;
+    }
+    Node node=(*map->base);
+    while(node!=NULL){
+        MapDataElement node_data=node->data;
+        MapKeyElement  node_key=node->key;
+        Node next_node=node->next;
+        map->freeData(node_data);
+        map->freeKey(node_key);
+        free(node);
+        node=next_node;
+
+    }
+    *map->base=NULL;
+    return MAP_SUCCESS;
+
+}
+
+void mapDestroy(Map map){
+    if(map==NULL){
+        return;
+    }
+    mapClear(map);
+    free(map);
+    return;
+}
+
+Map mapCopy(Map map){
+
 }
